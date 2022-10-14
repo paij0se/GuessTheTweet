cf5e8fce233577be581f7c716d2ae6cdd3fc56ee
Dr. Champeta <69026987+paij0se@users.noreply.github.com>
2022-10-14 17:40:57 -0500

    to my vps

--- web/script.js
+++ web/script.js
@@ -14,7 +14,7 @@ function sendAndReceive() {
     alert("Fill all the users!");
   } else {
     fetch(
-      `http://127.0.0.1:8080/tweets/?u=${tusername}&u2=${tusername2}&u3=${tusername3}&u4=${tusername4}`,
+      `http://170.187.200.5/tweets/?u=${tusername}&u2=${tusername2}&u3=${tusername3}&u4=${tusername4}`,
       {
         method: "GET",
       }
