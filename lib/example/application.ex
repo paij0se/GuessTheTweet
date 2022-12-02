45eabcb5113d60c02c295cdc899bdfd805483a4a
paij0se <hachikopolarcito321@gmail.com>
2022-12-01 23:54:06 -0500

    my bd

--- lib/example/application.ex
+++ lib/example/application.ex
@@ -4,12 +4,19 @@ defmodule Example.Application do
 
   def start(_type, _args) do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: Example.Router, options: [port: 8080]}
+      {Plug.Cowboy, scheme: :http, plug: Example.Router, options: [port: application_port()]}
     ]
+
     opts = [strategy: :one_for_one, name: Example.Supervisor]
 
-    Logger.info("http://127.0.0.1:8080")
+    Logger.info("http://127.0.0.1:#{application_port()}")
 
     Supervisor.start_link(children, opts)
+  end
+
+  defp application_port do
+    System.get_env()
+    |> Map.get("PORT", "8080")
+    |> String.to_integer()
   end
 end
